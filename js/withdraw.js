9c5ac88581d9d5bde255d4fc5d0d54332f090361
sharulislam <sharulislam60@gmail.com>
2023-02-28 23:57:07 +0600

    last commit

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,27 +5,45 @@ document.getElementById("btn-withdraw").addEventListener("click", function () {
   const newWithdrawAmountString = withdrawField.value;
   const newWithdrawAmount = parseFloat(newWithdrawAmountString); //convert string amount to number amount
 
+  
+  // step last
+  withdrawField.value = "";
+
+  if(isNaN(newWithdrawAmount)){
+    alert('please provide a number');
+    return;
+  }
+
   //step 3 :
   const withdrawtotalElement = document.getElementById("withdraw-total");
   const previousWithdrawTotalString = withdrawtotalElement.innerText;
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
   console.log(previousWithdrawTotal);
 
-  //step 4 :
-  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  //step 5  set total withdraw amount
-  withdrawtotalElement.innerText = currentWithdrawTotal;
+ 
 
   //step 6
   const balaceTotalElement = document.getElementById("total-bank-balance");
   const previousBalaceTotalString = balaceTotalElement.innerText;
   const previousBalanceTotal = parseFloat(previousBalaceTotalString);
+
+  
+
+  if(newWithdrawAmount>previousBalanceTotal){
+    alert('Not enough balance');
+    return;
+  }
+
+   //step 4 :
+   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+   //step 5  set total withdraw amount
+   withdrawtotalElement.innerText = currentWithdrawTotal;
+
   //step-7 calculate new balacne total
   const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 
   //set the value
   balaceTotalElement.innerText = newBalanceTotal;
 
-  // step last
-  withdrawField.value = "";
+  
 });
